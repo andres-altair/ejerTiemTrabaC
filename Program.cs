3116cd84ba7802b463c4c3157bb163c45e7e9b87
DESKTOP-JOOKDG6\profesor <csi23-amoliaz@colegioaltair.net>
2024-01-23 12:37:17 +0100

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace ejerTiemTrabaC;
+class Program
+{
+     static void Main(string[] args)
+    {
+        Console.WriteLine("minutos trabajados en un mes");
+        int min = Convert.ToInt32(Console.ReadLine());
+        decimal d = min / 1440;
+        decimal dia = Math.Truncate(d);
+        Console.WriteLine(dia);
+        decimal h  =min % 1440;
+        decimal horas = Math.Truncate(h);
+        Console.WriteLine(horas);
+        decimal m = h % horas;
+        decimal  minuto = m * 60;
+        Console.WriteLine(minuto);
+        
+
+    }
+
+
+
+}
